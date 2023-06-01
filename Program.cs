cf2300c427f2367f9cb0b52b695fbd9f07fa1e06
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-06-01 11:39:57 -0700

    Managing new commits

---
+++ Program.cs
@@ -0,0 +1,17 @@
+class Program
+{
+     public static void Main(string[] args)
+    {
+        Student s1 = new Student("Tom", 15,75);
+        s1.SayHello();
+
+        Student s2 = new Student("Fred", 15,95);
+        s2.SayHello();
+
+        Student s3 = new Student("Ted", 12,95);
+        s3.SayHello();
+    }
+       
+}
+
+    
---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿class Student
+{
+  
+    private int age;
+    private string name;
+    private double finalGrade;
+
+    public Student(string name, int age, double finalGrade)
+    {
+        this.name = name;
+        this.age = age;
+        this.finalGrade = finalGrade;
+    }
+
+    public Student(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello, my name is {name}, I am {age} years old, and my final grade is {finalGrade}.");
+    }
+}
